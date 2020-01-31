4f1f848058fb0829c17aaa37c744ebb530f6fbf1
Aleksey Priymachuk <a.priymachuk@limecs.net>
2020-01-31 20:26:36 +0200

    update

--- src/iframe.js
+++ src/iframe.js
@@ -3,28 +3,28 @@
 
 import ResizeObserver from 'resize-observer-polyfill';
 
-let padding = 0;
-const ro = new ResizeObserver((entries, observer) => {
-    for (const entry of entries) {
-        const {left, top, width, height} = entry.contentRect;
- 
-        // console.log('Element:', entry.target);
-        // console.log(`Element's size: ${ width }px x ${ height }px`);
-        // console.log(`Element's paddings: ${ top }px ; ${ left }px`);
+(function () {
+    if (window.top === window) {
+        // no iframe
+        return;
+    }
 
-        if (top === window) {
-            console.log('no iframe');
-            return;
-        }
-
+    function onResize() {
+        const {width, height} = document.body.parentElement.getBoundingClientRect();
         window.top.postMessage({
             type: 'resize',
-            height: height + padding,
-            width: width + padding,
+            height: height,
+            width: width,
         }, '*');
-
     }
-});
- 
-ro.observe(document.body.parentElement);
+
+    const ro = new ResizeObserver(onResize);
+    ro.observe(document.body.parentElement);
+
+    const inputs = document.querySelectorAll('input');
+    for (let i of inputs) {
+        i.addEventListener('change', onResize, false);
+    }
+
+})();
 
